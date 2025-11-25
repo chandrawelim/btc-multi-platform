96d6f7a73e90c903c728695ccd1742a138cc97ba
chandrawelim <chandra.welim@gmail.com>
2025-11-25 13:04:45 +0900

    Implement initial project structure and setup

---
+++ BTCKit/BTCKit/BTCKit.h
@@ -0,0 +1,18 @@
+//
+//  BTCKit.h
+//  BTCKit
+//
+//  Created by Welim, Chandra | ESDD on 2025/11/25.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for BTCKit.
+FOUNDATION_EXPORT double BTCKitVersionNumber;
+
+//! Project version string for BTCKit.
+FOUNDATION_EXPORT const unsigned char BTCKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BTCKit/PublicHeader.h>
+
+
