12530b3798ec6e9de9e13be9e8f3d6cb170ec641
chandrawelim <chandra.welim@gmail.com>
2026-02-25 15:31:30 +0900

    Add BTCKit framework to BTCCLI project; update project.pbxproj to include framework references and embed settings; add workspace configuration file.
    Initialize BTCCLI project with main.swift and project configuration files; add BTCPriceMonitor.swift to BTCKit for price monitoring functionality.

---
+++ BTCCLI/BTCCLI/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  BTCCLI
+//
+//  Created by Welim, Chandra | ESDD on 2026/02/25.
+//
+
+import Foundation
+
+print("Hello, World!")
+
---
+++ BTCKit/BTCKit/BTC Price Feature/BTCPriceMonitor.swift
@@ -0,0 +1,8 @@
+//
+//  BTCPriceMonitor.swift
+//  BTCKit
+//
+//  Created by Welim, Chandra | ESDD on 2026/02/25.
+//
+
+import Foundation
